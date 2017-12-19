6636a1edf4fd9b171a934a9ebd4997c0ded9e3f1
hongguang.zhou <hongguang.zhou@rokid.com>
2017-12-19 11:37:23 +0800

    deep copy function added

--- swiftLearning/swiftLearning/FirstViewController.swift
+++ swiftLearning/swiftLearning/FirstViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
+
+
+
 class FirstViewController: UIViewController {
 
+    func  prototypePattern()
+    {
+        var people = [Person(name:"Joe", country:"france"), Person(name:"Bob", country:"USA")]
+        var otherPeople = people
+        people[0].country = "UK"
+        print("country:  \(otherPeople[0].country)")
+        
+        
+//        people.map { (item) -> Person in
+//            if (item is NSCopying)
+//            item.deepCopy(data: <#T##[AnyObject]#>)
+//        }
+        var anotherPeople = deepCopy(data: people) as! [Person]
+        
+        people[0].country = "unknown"
+        print("country: \(anotherPeople[0].country)")
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.prototypePattern()
+        
     }
 
     override func didReceiveMemoryWarning() {
--- swiftLearning/swiftLearning/Person.swift
+++ swiftLearning/swiftLearning/Person.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+
+class Person : NSObject, NSCopying {
+
+    var name: String
+    var country:String
+    
+    init(name:String, country:String)
+    {
+        self.name = name
+        self.country = country
+    }
+
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Person(name:self.name, country: self.country)
+    }
+    
+    
+    func test()
+    {
+        print("this is test func")
+    }
+}
+
+
+func deepCopy(data:[AnyObject]) -> [AnyObject]
+{
+    return data.map({ (item) -> AnyObject in
+        if (item is NSCopying && item is NSObject)
+        {
+            return (item as! NSObject).copy() as AnyObject;
+        }else
+        {
+            return item
+        }
+    })
+}
+
+
+

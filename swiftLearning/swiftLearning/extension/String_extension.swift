ac5992842e3d21fcb83e8ff3185a86054431af53
hongguang.zhou <hongguang.zhou@rokid.com>
2017-12-19 09:33:55 +0800

    String extension added

--- swiftLearning/swiftLearning/extension/String_extension.swift
+++ swiftLearning/swiftLearning/extension/String_extension.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+
+extension String
+{
+    public func deleteUnexpectedCharacters() -> String {
+        let str = self.trimmingCharacters(in: .whitespaces)
+        let characterSet = CharacterSet(charactersIn: "若琪，,：:；; ")
+        let strTmp = str.trimmingCharacters(in: characterSet)
+        return strTmp
+    }
+    
+    public func getWords()->String
+    {
+        var words: String {
+            return components(separatedBy: .punctuationCharacters)
+                .joined()
+                .components(separatedBy: .whitespacesAndNewlines)
+                .joined()
+                .components(separatedBy: .controlCharacters)
+                .joined()
+                .components(separatedBy: .nonBaseCharacters)
+                .joined()
+                .components(separatedBy: .illegalCharacters)
+                .joined()
+                .components(separatedBy: .symbols)
+                .filter{!$0.isEmpty}
+                .joined()
+        }
+        return words
+    }
+    
+    
+
+}

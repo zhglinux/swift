4db893909a1884c2eedb7530aaee301f76fe79e4
hongguang.zhou <hongguang.zhou@rokid.com>
2017-12-19 09:31:49 +0800

    create learning  project

---
+++ swiftLearning/swiftLearning/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  swiftLearning
+//
+//  Created by coco zhou on 2017/12/19.
+//  Copyright © 2017年 coco zhou. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

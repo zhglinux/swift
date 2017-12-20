e6bc39b9f005943f9b7d0565581a498e9501e664
hongguang.zhou <hongguang.zhou@rokid.com>
2017-12-20 14:30:16 +0800

    改变目录结构，修改代码

--- swiftLearning/swiftLearning/FirstViewController.swift
+++ swiftLearning/swiftLearning/FirstViewController.swift
@@ -13,71 +13,26 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    func  prototypePattern()
-    {
-        var people = [Person(name:"Joe", country:"france"), Person(name:"Bob", country:"USA")]
-        var otherPeople = people
-        people[0].country = "UK"
-        print("country:  \(otherPeople[0].country)")
-        
-        //
-        let arr0 = people.map { (item) -> Person in
-            item.deepCopy()
-        }
-        print(arr0)
-        
-        //
-        var arr = people.map { (item) -> [Person] in
-            var tmp = [Person]()
-            tmp.append(item.deepCopy())
-            return tmp
-        }
-        print(arr)
-        arr[0][0].country = "arr"
-        
-        //
-        var arr2 = people.map({ $0.deepCopy() })
-        print(arr2)
-        arr2[0].country = "test"
-        
-        //
-        var anotherPeople = openDeepCopy(data: people) as! [Person]
-        people[0].country = "unknown"
-        print("country: \(anotherPeople[0].country)")
-        
-        
-    }
-    
-    //这个是实例方法
-    func result()
-    {
-        var  calc1 = Sum(first: 3, second: 8).Result
-        print("calc1 = \(calc1)")
-        var  calc2 = Sum(first: 0, second: 9).Result
-        print("calc2 = \(calc2)")
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+  
+        //prototype pattern
+        PP_Test().testEntry()
         
-        testAppointment_CC_NN()
-        testAppointment_CC()
-        testAppoint()
         
         
+        
+        //
         print ( x(view: self.view) )
         print(GlobalConst.kScreenWidth)
         print(kScreenWidth)
+        //
         
         
-        Sum.result()    //类方法
-        self.result()   // 实例方法
-        globalResult()  //全局函数
         
         
-        self.prototypePattern()
         
     }
 
--- swiftLearning/swiftLearning/PrototypePattern/PP_Test.swift
+++ swiftLearning/swiftLearning/PrototypePattern/PP_Test.swift
@@ -7,3 +7,85 @@
 //
 
 import Foundation
+
+class PP_Test //PrototypePattern
+{
+    init() {
+        
+    }
+    func PP_Test()
+    {
+        print("when does this func exec")//not exec when constructing
+    }
+    
+    func  prototypePattern()
+    {
+        var people = [Person(name:"Joe", country:"france"), Person(name:"Bob", country:"USA")]
+        var otherPeople = people
+        people[0].country = "UK"
+        print("country:  \(otherPeople[0].country)")
+        
+        //
+        let arr0 = people.map { (item) -> Person in
+            item.deepCopy()
+        }
+        print(arr0)
+        
+        //
+        var arr = people.map { (item) -> [Person] in
+            var tmp = [Person]()
+            tmp.append(item.deepCopy())
+            return tmp
+        }
+        print(arr)
+        arr[0][0].country = "arr"
+        
+        //
+        var arr2 = people.map({ $0.deepCopy() })
+        print(arr2)
+        arr2[0].country = "test"
+        
+        //
+        var anotherPeople = openDeepCopy(data: people) as! [Person]
+        people[0].country = "unknown"
+        print("country: \(anotherPeople[0].country)")
+        
+        
+    }
+    
+    //这个是实例方法
+    func result()
+    {
+        var  calc1 = Sum(first: 3, second: 8).Result
+        print("calc1 = \(calc1)")
+        var  calc2 = Sum(first: 0, second: 9).Result
+        print("calc2 = \(calc2)")
+        
+    }
+    
+    
+    //
+    func testEntry()
+    {
+        
+        testAppointment_CC_NN_LN()
+        testAppointment_CC_NN_L()
+        testAppointment_CC_NN()
+        testAppointment_CC()
+        testAppoint()
+        
+        Sum.result()    //类方法
+        self.result()   // 实例方法
+        globalResult()  //全局函数
+        
+        
+        self.prototypePattern()
+        
+        
+        
+    }
+    
+    
+    
+}
+

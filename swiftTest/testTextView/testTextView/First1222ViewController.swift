fcfb49f8b4de2322edca313d79b706ff66fd00c3
hongguang.zhou <hongguang.zhou@rokid.com>
2018-01-08 19:21:02 +0800

    add new files

---
+++ swiftTest/testTextView/testTextView/First1222ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  First1222ViewController.swift
+//  testTextView
+//
+//  Created by coco zhou on 2018/1/8.
+//  Copyright © 2018年 coco zhou. All rights reserved.
+//
+
+import UIKit
+
+class First1222ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
